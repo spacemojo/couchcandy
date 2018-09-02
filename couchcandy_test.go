ef3d8647946f612063dd750809da2167e08dd7a0
Frederic Leclerc <fred.leclerc@gmail.com>
2018-09-02 13:26:21 -0400

    Corrected spelling mistakes in unit tests

--- couchcandy_test.go
+++ couchcandy_test.go
@@ -132,7 +132,7 @@ func TestDeleteFailure(t *testing.T) {
 		Host: "http://127.0.0.1", Port: 5984, Database: "lendr", Username: "test", Password: "gotest",
 	})
 	couchcandy.DeleteHandler = func(string) (resp *http.Response, e error) {
-		return nil, fmt.Errorf("an error occured when deleting the document")
+		return nil, fmt.Errorf("an error occurred when deleting the document")
 	}
 
 	_, err := couchcandy.Delete("053cc05f2ee97a0c91d276c9e700194b", "3-b96f323b37f19c4d1affddf3db3da9c5")
@@ -516,7 +516,7 @@ func TestDocumentsByKeysFailure(t *testing.T) {
 		Host: "http://127.0.0.1", Port: 5984, Database: "userapi", Username: "test", Password: "pwd",
 	})
 	couchcandy.PostHandler = func(string, string) (*http.Response, error) {
-		return nil, fmt.Errorf("An error occured when fetching documents by keys")
+		return nil, fmt.Errorf("An error occurred when fetching documents by keys")
 	}
 
 	_, err := couchcandy.DocumentsByKeys([]string{""}, Options{IncludeDocs: true, Limit: 10})
@@ -616,7 +616,7 @@ func TestCallMapFunctionFailure(t *testing.T) {
 		Host: "http://127.0.0.1", Port: 5984, Database: "lendr", Username: "test", Password: "gotest",
 	})
 	couchcandy.GetHandler = func(string) (*http.Response, error) {
-		return nil, fmt.Errorf("an error occured whilst calling the map function")
+		return nil, fmt.Errorf("an error occurred whilst calling the map function")
 	}
 
 	docs, err := couchcandy.View("cards", "by_suit", Options{})
